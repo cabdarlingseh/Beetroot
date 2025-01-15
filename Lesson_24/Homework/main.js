65757d064be4d7a00d4d732d370963e1e4f5a471
cabdarlingseh <david_cj7@protonmail.com>
2025-01-15 16:23:11 +0000

    weather app

--- Lesson_24/Homework/main.js
+++ Lesson_24/Homework/main.js
@@ -1,12 +1,18 @@
 const API_Key = '699838e9c691cfc877855ed97ad10689';
 
 
+//Fetching the weather data
+
 function fetchWeather(cityName) {
     const URL = `https://api.openweathermap.org/data/2.5/find?q=${cityName}&appid=${API_Key}&units=metric`;
 
     fetch(URL)
         .then(res => res.json())
         .then(response => {
+
+            if (!response.list || response.list.length === 0 || response.list === isNaN) {
+                alert('City not found. Please enter a  valid city name.')
+            }
             const theWeather = response?.list[0].main;
 
             const card = document.querySelector('.weather_card');
@@ -14,28 +20,34 @@ function fetchWeather(cityName) {
             if (card) {
                 card.style.display = 'block';
             }
+
+            //Upadating the weather details
+
             currentWeather(theWeather);
             feelsLike(theWeather);
             humidity(theWeather);
             max_temp(theWeather);
             min_temp(theWeather);
-            // console.log(theWeather);
+            getWeatherConditions(cityName);
+            getWeatherIcon(cityName);
 
-
+            const placeName = document.querySelector('.cityName');
+            placeName.textContent = cityName;
+        })
+        .catch(err => {
+            console.log(err);
         })
 }
+
+//Getting the name of the city 
 
 function searchWeather() {
     const cityName = document.querySelector('#city').value || 'Lisbon';
 
     fetchWeather(cityName);
-    getWeatherConditions(cityName);
-
-    const placeName = document.querySelector('.cityName');
-    placeName.textContent = cityName;
-
-
 }
+
+//Adding an event so that the search is activated when the enter key is pressed
 
 const enter_button = document.querySelector('input[type ="text"]');
 
@@ -44,33 +56,36 @@ enter_button.addEventListener('keypress', function (event) {
         event.preventDefault();
         searchWeather();
     };
-})
+});
+
+//Functions to update the weather details
 
 function currentWeather(data) {
     const temp_wrapper = document.querySelector('.temperature');
-    temp_wrapper.textContent = Math.round(data.temp);
-    // console.log(temp_wrapper);
+    temp_wrapper.textContent = `${Math.round(data.temp)}°C`;
 }
 
 function feelsLike(data) {
     const feeling = document.querySelector('.feelslike');
-    feeling.textContent = Math.round(data.feels_like);
+    feeling.textContent = `Feels like: ${Math.round(data.feels_like)}°C`;
 }
 
 function humidity(data) {
     const humid = document.querySelector('.humidity');
-    humid.textContent = Math.round(data.humidity);
+    humid.textContent = `Humidity: ${Math.round(data.humidity)}%`;
 }
 
 function max_temp(data) {
     const max_temperature = document.querySelector('.max_temp');
-    max_temperature.textContent = Math.round(data.temp_max);
+    max_temperature.textContent = `Max-Temp: ${Math.round(data.temp_max)}°C`;
 }
 
 function min_temp(data) {
     const min_temperature = document.querySelector('.min_temp');
-    min_temperature.textContent = Math.round(data.temp_min);
+    min_temperature.textContent = `Min-Temp: ${Math.round(data.temp_min)}°C`;
 }
+
+//Function to get the weather conditions
 
 function getWeatherConditions(cityName) {
     const URL = `https://api.openweathermap.org/data/2.5/find?q=${cityName}&appid=${API_Key}&units=metric`;
@@ -84,11 +99,33 @@ function getWeatherConditions(cityName) {
             // console.log(mainWeather);
 
             const conditions = document.querySelector('.conditions');
-            conditions.textContent = mainWeather;
+            conditions.textContent = `Current conditions: ${mainWeather}`;
 
         })
 
 }
+
+//Getting the weather icon
+
+function getWeatherIcon(cityName) {
+    const URL = `https://api.openweathermap.org/data/2.5/find?q=${cityName}&appid=${API_Key}&units=metric`;
+
+    fetch(URL)
+        .then(res => res.json())
+        .then(response => {
+            const the_icon = response?.list[0].weather[0].icon;
+
+            const iconUrl = `https://openweathermap.org/img/wn/${the_icon}@2x.png`;
+
+            const weatherIcon = document.querySelector('.weather_icon');
+            weatherIcon.src = iconUrl;
+
+            console.log(the_icon);
+
+        })
+}
+
+//Changing the background according to the weather conditions
 
 function changeBackground(weatherCondition) {
     const body = document.body;
@@ -122,3 +159,10 @@ function changeBackground(weatherCondition) {
 
     }
 }
+
+// Making sure that the default city is always displayed
+
+document.addEventListener('DOMContentLoaded', function () {
+    const defaultCity = 'Lisbon';
+    fetchWeather(defaultCity);
+})
