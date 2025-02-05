cbf2e05b803484503e9f25940b3eeff59e8ed3db
cabdarlingseh <david_cj7@protonmail.com>
2025-02-05 17:07:02 +0000

    changed the navbar to be in fixed position

--- Project_2/main.js
+++ Project_2/main.js
@@ -1,4 +1,20 @@
+// For the Scrolling of the title bar
+
+window.addEventListener('scroll', function () {
+    let titleBar = document.querySelector('.title_bar');
+    if (window.scrollY > 50) {
+        titleBar.classList.add('scrolled');
+        titleBar.style.paddingTop = "0";
+    } else {
+        titleBar.classList.remove('scrolled');
+        titleBar.style.paddingTop = "53px";
+    }
+})
+
 $(document).ready(function () {
+
+    //    Changing the main texts JQuery
+
     $('.header_body').slick({
         slidesToScroll: 1,
         arrows: false,
